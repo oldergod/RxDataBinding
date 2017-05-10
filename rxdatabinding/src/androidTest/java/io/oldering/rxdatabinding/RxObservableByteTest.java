ee4aed3a6f8db3e473b0ee27f702c33c5c8bb72c
benoit <oldering@gmail.com>
2017-05-10 12:39:19 +0900

    removed useless warning suppresion

--- rxdatabinding/src/androidTest/java/io/oldering/rxdatabinding/RxObservableByteTest.java
+++ rxdatabinding/src/androidTest/java/io/oldering/rxdatabinding/RxObservableByteTest.java
@@ -17,7 +17,6 @@ public class RxObservableByteTest {
 
   private final ObservableByte observableByte = new ObservableByte();
 
-  @SuppressWarnings("ConstantConditions") //
   @Test @UiThreadTest public void propertyChangeEvents() {
     byte value = 1;
 
